79c0b870d590ac3f7f7b4348382af804ab4fa681
stellaria <xpwanghy@hotmail.com>
2019-06-05 21:13:59 +0800

    2019年6月5日 21:11:06

---
+++ src/main/java/com/lunacia/filemanager/FileManagerApplication.java
@@ -0,0 +1,13 @@
+package com.lunacia.filemanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FileManagerApplication.class, args);
+	}
+
+}
